be7369599cd2d4b812bac0d99da99f40d4cb21d5
Kim Berame <kimroland.berame.21@usjr.edu.ph>
2025-05-03 12:31:49 +0800

    removed firebase from techstack, refactor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,17 @@
-"use client"
+"use client";
 import { useState } from "react";
 
-import AuthForm from "@/components/landing_page/AuthForm";
 import Header from "@/components/landing_page/Header";
 import NavBar from "@/components/NavBar";
-import { loginUser, loginWithGoogle } from "@/lib/firebaseAuth";
 
 export default function Home() {
-    const [loading, setIsLoading] = useState(false)
-
-    const handleLogin = async (loginData: { email: string, password: string }) => {
-        setIsLoading(true);
-        try {
-            await loginUser(loginData.email, loginData.password);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
-    const handleGoogleLogin = async () => {
-        setIsLoading(true);
-        try {
-            await loginWithGoogle();
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
-    }
-
-    return (
-        <div className="flex h-svh flex-col">
-            <NavBar />
-            <div className="flex flex-row">
-                <AuthForm
-                    isLoading={loading}
-                    handleLogin={handleLogin}
-                    handleGoogleLogin={handleGoogleLogin}
-                />
-                <Header />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex h-screen flex-col">
+      {/* move this to layout */}
+      <NavBar />
+      <div className="centered">
+        <Header />
+      </div>
+    </div>
+  );
 }
--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,19 +1,18 @@
-//* LoadingOverlay.js
-import React from 'react'
-import styles from './LoadingOverlay.module.css'
+import React from "react";
+import styles from "./LoadingOverlay.module.css";
 
 interface LoadingOverlayProps {
-    isLoading: boolean;
+  isLoading: boolean;
 }
 
 const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ isLoading }) => {
-    return (
-        isLoading && (
-            <div className={styles.overlay}>
-                <div className={styles.spinner}></div>
-            </div>
-        )
-    );
+  return (
+    isLoading && (
+      <div className={styles.overlay}>
+        <div className={styles.spinner}></div>
+      </div>
+    )
+  );
 };
 
 export default LoadingOverlay;
